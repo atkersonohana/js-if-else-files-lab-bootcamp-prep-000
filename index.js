20ff721cd984ed8924a3e38f3ca909ce2b4595cb
Jamey Atkerson <atkersonohana@gmail.com>
2018-07-29 13:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var name = "Joe"
 var height = "74"
 if (name==="Susan"){
-  console.og("The name is correct")}
+  console.log("The name is correct")}
 
 // Don't worry about this
 module.exports = { name, height
-}
+} else {
+  console.log("Expected the name to be Susan")}
 
